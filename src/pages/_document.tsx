fbc6c1f6f39dc2d34a7e1d350fb284dc50bdd2e0
Keenan Schouten <securearms.project@gmail.com>
2025-05-04 17:29:19 -0600

    feat: add Plausible + GTM integration and static export config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// src/pages/_document.tsx
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* ✅ Plausible Analytics for Static Export */}
+          <script
+            defer
+            data-domain="brassroots.market"
+            src="https://plausible.io/js/plausible.js"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
